40875a980ed27ea88fbfc0ab57998cdc3bd1edd9
muhammadramadan <medo44448888@gmail.com>
2025-10-14 21:35:23 +0300

    feat(recent-posts): Integrate localization for recent posts section with dynamic title

--- components/layout/recentPosts.tsx
+++ components/layout/recentPosts.tsx
@@ -1,13 +1,16 @@
 import { getAllMdxFiles } from "@/lib/mdx";
 import PostCard from "../ui/postCard";
 import { Post } from "@/types/blog";
+import { getLocale, getTranslations } from "next-intl/server";
 
 export default async function RecentPosts() {
-  const posts: Post[] = await getAllMdxFiles();
+  const t = await getTranslations("home.recentPosts");
+  const locale = await getLocale();
+  const posts: Post[] = await getAllMdxFiles(locale);
   const recentPosts = posts.slice(0, 3);
   return (
     <section data-fade-3>
-      <h2 className="text-foreground mb-2 font-medium">Recent posts</h2>
+      <h2 className="text-foreground mb-2 font-medium">{t("title")}</h2>
       <ul className="">
         {recentPosts.map((post) => (
           <li key={post.slug}>
