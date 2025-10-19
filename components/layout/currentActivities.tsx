07ff31b609c4f317b51a86845a4e818115a722eb
muhammadramadan <medo44448888@gmail.com>
2025-10-19 22:07:19 +0300

    feat(current-activities): Add CurrentActivities component with localization and external links

---
+++ components/layout/currentActivities.tsx
@@ -0,0 +1,32 @@
+import { useTranslations } from "next-intl";
+import ExternalLink from "../ui/externalLink";
+import { Link } from "@/i18n/routing";
+
+export default function CurrentActivities() {
+  const t = useTranslations("home.currentActivities");
+  return (
+    <section data-fade-2>
+      <h2 className="text-foreground mb-2 font-medium">{t("title")}</h2>
+      <ul className="marker:text-dark-muted dark:marker:text-light-muted list-disc ltr:pl-4 rtl:pr-4">
+        {t.rich("content", {
+          l1: (chunks) => <li>{chunks}</li>,
+          l2: (chunks) => <li>{chunks}</li>,
+          l3: (chunks) => <li>{chunks}</li>,
+          call: (chunks) => (
+            <ExternalLink
+              href="https://cal.com/mohamedramadan"
+              className="link inline-block"
+            >
+              {chunks}
+            </ExternalLink>
+          ),
+          bucketlist: (chunks) => (
+            <Link prefetch href="/bucket-list" className="link">
+              {chunks}
+            </Link>
+          ),
+        })}
+      </ul>
+    </section>
+  );
+}
