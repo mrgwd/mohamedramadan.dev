6ecfa7677be8cb6b3641301552c44689e23f783e
muhammadramadan <medo44448888@gmail.com>
2025-10-30 15:35:29 +0300

    feat(recent-posts): Enhance RecentPosts component to format post dates and improve title rendering

--- components/layout/recentPosts.tsx
+++ components/layout/recentPosts.tsx
@@ -2,6 +2,7 @@ import { getAllMdxFiles } from "@/lib/mdx";
 import PostCard from "../ui/postCard";
 import { Post } from "@/types/blog";
 import { getLocale, getTranslations } from "next-intl/server";
+import { formatDate } from "@/lib/utils";
 
 export default async function RecentPosts() {
   const t = await getTranslations("home.recentPosts");
@@ -11,11 +12,18 @@ export default async function RecentPosts() {
   return (
     <section data-fade-3>
       <h2 className="text-foreground mb-2 font-medium">{t("title")}</h2>
-      <ul className="">
+      <ul>
         {recentPosts.map((post) => (
           <li key={post.slug}>
             <PostCard href={post.slug} date={post.frontmatter.createdAt}>
-              {post.frontmatter.title}
+              <PostCard.Title>{post.frontmatter.title}</PostCard.Title>
+              <PostCard.PostCardDate>
+                {formatDate({
+                  date: post.frontmatter.createdAt,
+                  format: "short",
+                  locale: locale === "ar" ? "ar-SA" : "en-US",
+                })}
+              </PostCard.PostCardDate>
             </PostCard>
           </li>
         ))}
