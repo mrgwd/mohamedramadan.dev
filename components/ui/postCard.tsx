faed2705f1ad8ee36b2b7ed95f18ea07e2d7f86c
muhammadramadan <medo44448888@gmail.com>
2025-11-01 21:47:54 +0200

    refactor(postCard): Simplify PostCard component by removing date formatting and adding PostCardDate subcomponent for better structure

--- components/ui/postCard.tsx
+++ components/ui/postCard.tsx
@@ -1,27 +1,49 @@
 import { Link } from "@/i18n/routing";
-import { formatDate } from "@/lib/utils";
-import { useLocale } from "next-intl";
+import { cn } from "@/lib/utils";
+
 interface PostCardProps extends React.ComponentPropsWithoutRef<"a"> {
   href: string;
   children: React.ReactNode;
   date: string;
 }
-function PostCard({ href, children, date }: PostCardProps) {
-  const locale = useLocale();
-  const hijri = locale === "ar" ? true : false;
+function PostCard({ href, children }: PostCardProps) {
   return (
     <Link prefetch href={`/blog/${href}`} className="group my-2 block">
-      <PostCardTitle>{children}</PostCardTitle>
-      <p className="text-muted-foreground-subtle">{formatDate(date, hijri)}</p>
+      {children}
     </Link>
   );
 }
-function PostCardTitle({ children }: { children: React.ReactNode }) {
+function PostCardTitle({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) {
   return (
-    <h2 className="decoration-transparent underline-offset-2 transition-all duration-300 group-hover:underline group-hover:decoration-inherit">
+    <h2
+      className={cn(
+        "decoration-transparent underline-offset-2 transition-all duration-300 group-hover:underline group-hover:decoration-inherit",
+        className,
+      )}
+    >
       {children}
     </h2>
   );
 }
-
+function PostCardDate({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <p className={cn("text-muted-foreground-subtle text-sm", className)}>
+      {children}
+    </p>
+  );
+}
+PostCard.Title = PostCardTitle;
+PostCard.PostCardDate = PostCardDate;
 export default PostCard;
