933b7c602c85c84afd35b0d69090e76936927fef
muhammadramadan <medo44448888@gmail.com>
2025-11-01 22:01:18 +0200

    feat(stats): Enhance Stats component with linkable like and view counts for improved accessibility

--- components/layout/blog/stats.tsx
+++ components/layout/blog/stats.tsx
@@ -1,5 +1,6 @@
 import { EyeIcon, HeartIcon } from "@phosphor-icons/react/dist/ssr";
 import { useFormatter, useTranslations } from "next-intl";
+import Link from "next/link";
 
 export default function Stats({
   likes,
@@ -17,16 +18,28 @@ export default function Stats({
     numberingSystem: "latn",
   });
   return (
-    <div className="force-latin-digits flex gap-2 *:flex *:items-center *:gap-1">
-      <p>
-        <HeartIcon />
+    <div className="flex gap-2 *:flex *:items-center *:gap-1">
+      <Link href="#like-button-section" className="link-decoration">
+        <HeartIcon aria-hidden="true" />
+        <span className="sr-only">
+          {t("common.likeCount", {
+            count: formattedLikes,
+            formattedCount: formattedLikes,
+          })}
+        </span>
         {t("common.likeCount", {
           count: formattedLikes,
           formattedCount: formattedLikes,
         })}
-      </p>
+      </Link>
       <p>
-        <EyeIcon />
+        <EyeIcon aria-hidden="true" />
+        <span className="sr-only">
+          {t("common.viewCount", {
+            count: formattedViews,
+            formattedCount: formattedViews,
+          })}
+        </span>
         {t("common.viewCount", {
           count: formattedViews,
           formattedCount: formattedViews,
