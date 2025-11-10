3ecb4a432994744457fb946f478912561b104fdd
muhammadramadan <medo44448888@gmail.com>
2025-11-10 14:27:01 +0200

    feat(footer): Update footer component to use dynamic social links and translations

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,19 +1,29 @@
+import { SOCIALS } from "@/constants/social";
 import ExternalLink from "../ui/externalLink";
+import { useTranslations } from "next-intl";
 
 export default function Footer() {
+  const t = useTranslations();
   return (
-    <footer className="text-foreground-secondary-muted my-8 flex flex-wrap gap-x-3">
-      <ExternalLink href="https://twitter.com/_MuhammedR">
-        @_MuhammedR
+    <footer className="layout mt-16 mb-8 flex flex-wrap gap-x-3" data-fade-4>
+      <ExternalLink href={`https://twitter.com/${SOCIALS.twitter}`}>
+        {t("footer.twitter", { handle: SOCIALS.twitter })}
       </ExternalLink>
-      <ExternalLink href="https://linkedin.com/in/mrgwd">linkedin</ExternalLink>
-      <ExternalLink href="https://github.com/mrgwd">github</ExternalLink>
-      <ExternalLink href="mailto:mogdwd@gmail.com">email</ExternalLink>
+      <ExternalLink href={`https://linkedin.com/in/${SOCIALS.linkedin}`}>
+        {t("common.socialMedia.linkedin")}
+      </ExternalLink>
+      <ExternalLink href={`https://github.com/${SOCIALS.github}`}>
+        {t("common.socialMedia.github")}
+      </ExternalLink>
+      <ExternalLink href={`mailto:${SOCIALS.email}`}>
+        {t("common.socialMedia.email")}
+      </ExternalLink>
+
       <ExternalLink
-        className="sm:ml-auto"
-        href="https://github.com/mrgwd/muhammadramadan"
+        className="ltr:sm:ml-auto rtl:sm:mr-auto"
+        href={`https://github.com/${SOCIALS.github}/mohamedramadan.dev`}
       >
-        source code
+        {t("footer.sourceCode")}
       </ExternalLink>
     </footer>
   );
