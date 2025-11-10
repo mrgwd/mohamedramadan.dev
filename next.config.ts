e28c86745ae16217febdb5c7b995010cc26725e9
muhammadramadan <medo44448888@gmail.com>
2025-11-10 16:28:37 +0200

    feat(config): Add remote image patterns for Cloudinary in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,19 @@ import createMDX from "@next/mdx";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-  experimental: { optimizePackageImports: ["phosphor-icons/react"] },
+  experimental: {
+    optimizePackageImports: ["phosphor-icons/react"],
+  },
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**/**", // Allows all paths on the domain
+      },
+    ],
+  },
 };
 const withMDX = createMDX({});
 const withNextIntl = createNextIntlPlugin();
